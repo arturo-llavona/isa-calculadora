c214886e169c81ce1f7fb9bcedd1d69c9c4e87b4
Arturo Llavona Vallina <allavona@paradigmadigital.com>
2020-05-20 17:56:40 +0200

    Añadimos la calculadora sin lógica, y comenzamos con los tests

---
+++ src/main/java/net/unir/allavona/isa/Calculadora.java
@@ -0,0 +1,20 @@
+package net.unir.allavona.isa;
+
+public class Calculadora {
+	
+	public Integer suma(Integer a, Integer b ) {
+		return null;
+	}
+	public Integer resta(Integer a, Integer b) {
+		return null;
+	}
+	public Integer multiplica(Integer a, Integer b) {
+		return null;
+	}
+	public Double divide(Integer a, Integer b) {
+		return null;
+	}
+	public Double raizCuadrada(Integer a) {
+		return null;
+	}
+}
--- src/test/java/net/unir/allavona/isa/CalculadoraTest.java
+++ src/test/java/net/unir/allavona/isa/CalculadoraTest.java
@@ -2,13 +2,46 @@ package net.unir.allavona.isa;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculadoraTest {
-
+	public static Calculadora calculadora;
+	
+	@Before
+	public void init() {
+		calculadora = new Calculadora();
+	}
+	
 	@Test
-	public void test1() {
-		assertEquals(2, 2);
-		assertEquals(2, 1);
+	public void testSuma() {
+		// Comprobamos la suma básica
+		assertEquals(Integer.valueOf(7), calculadora.suma(5, 2));
+		
+		// Comprobamos la propiedad conmutativa
+		Integer a = calculadora.suma(5, 2);
+		Integer b = calculadora.suma(2, 5);
+		
+		assertEquals(a, b);		
+	}
+	
+	@Test
+	public void testResta() {
+		
+	}
+	
+	@Test
+	public void testMultiplicacion() {
+		
+	}
+	
+	@Test
+	public void testDivision() {
+		
+	}
+	
+	@Test
+	public void testRaizCuadrada() {
+		
 	}
 }
