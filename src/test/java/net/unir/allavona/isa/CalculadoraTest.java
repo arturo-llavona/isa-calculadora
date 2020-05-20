03ed5fe7d7c4a3b8396eca81495770c801c6ce76
Arturo Llavona Vallina <allavona@paradigmadigital.com>
2020-05-20 17:35:33 +0200

    Se añaden librerías de JUnit 4.13 y un primer test de prueba

---
+++ src/test/java/net/unir/allavona/isa/CalculadoraTest.java
@@ -0,0 +1,14 @@
+package net.unir.allavona.isa;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CalculadoraTest {
+
+	@Test
+	public void test1() {
+		assertEquals(2, 2);
+		assertEquals(2, 1);
+	}
+}
