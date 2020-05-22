4e00561b1c187b2b3742454768d64afb2e7d3f9d
Arturo Llavona Vallina <allavona@paradigmadigital.com>
2020-05-22 19:33:33 +0200

    formateo del código

--- src/main/java/net/unir/allavona/isa/CalculadoraMain.java
+++ src/main/java/net/unir/allavona/isa/CalculadoraMain.java
@@ -5,36 +5,37 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class CalculadoraMain {
-    public static void main(String[] args) throws IOException {
-    	printMenu();
-    	int returned = 0;
-    	do {
-    		returned = askOperation();
-    	} while ( returned != -1 );
-    	System.out.println("Fin calculadora");
-    }
-    public static void printMenu() {
-    	System.out.println("Calculadora");
-    	System.out.println("-----------------------------------------");
-    	System.out.println("Opciones disponibles: ");
-    	System.out.println("\t - Suma (Ejemplo: 5 + 5)");
-    	System.out.println("\t - Resta (Ejemplo: 5 - 5)");
-    	System.out.println("\t - División (Ejemplo: 5 / 5)");
-    	System.out.println("\t - Multiplicación (Ejemplo: 5 * 5)");
-    	System.out.println("\t - Raiz cuadrada (Ejemplo: raiz 9)");
-    	System.out.println("\t - Salir");
-    	
-    }
-    public static int askOperation() throws IOException  {
-    	System.out.print("Introduce la operación: ");
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	String operacion = br.readLine();
-    	if ( operacion.equalsIgnoreCase("SALIR") ) {
-    		return -1;
-    	} else {
-    		System.out.println("Resultado: " + CalculadoraHelper.calcular(operacion));
-    		return 0;
-    	}
-    }
-}
+	public static void main(String[] args) throws IOException {
+		printMenu();
+		int returned = 0;
+		do {
+			returned = askOperation();
+		} while (returned != -1);
+		System.out.println("Fin calculadora");
+	}
 
+	public static void printMenu() {
+		System.out.println("Calculadora");
+		System.out.println("-----------------------------------------");
+		System.out.println("Opciones disponibles: ");
+		System.out.println("\t - Suma (Ejemplo: 5 + 5)");
+		System.out.println("\t - Resta (Ejemplo: 5 - 5)");
+		System.out.println("\t - División (Ejemplo: 5 / 5)");
+		System.out.println("\t - Multiplicación (Ejemplo: 5 * 5)");
+		System.out.println("\t - Raiz cuadrada (Ejemplo: raiz 9)");
+		System.out.println("\t - Salir");
+
+	}
+
+	public static int askOperation() throws IOException {
+		System.out.print("Introduce la operación: ");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String operacion = br.readLine();
+		if (operacion.equalsIgnoreCase("SALIR")) {
+			return -1;
+		} else {
+			System.out.println("Resultado: " + CalculadoraHelper.calcular(operacion));
+			return 0;
+		}
+	}
+}
--- src/test/java/net/unir/allavona/isa/CalculadoraTest.java
+++ src/test/java/net/unir/allavona/isa/CalculadoraTest.java
@@ -7,91 +7,97 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class CalculadoraTest {
-	
+
 	@Before
 	public void init() {
-		
+
 	}
-	
+
 	@Test
 	public void testSuma() {
 		// Comprobamos la suma básica
 		assertEquals(new Integer(7), Calculadora.suma(5, 2));
-		
+
 		// Comprobamos la propiedad conmutativa
 		assertEquals(Calculadora.suma(5, 2), Calculadora.suma(2, 5));
-		
-		// Comprobamos la suma de un número positivo con otro negativo cuyo resultado es positivo
+
+		// Comprobamos la suma de un número positivo con otro negativo cuyo resultado es
+		// positivo
 		assertEquals(new Integer(2), Calculadora.suma(5, -3));
-		
-		// Comprobamos la suma de un número positivo con otro negativo cuyo resultado es negativo
+
+		// Comprobamos la suma de un número positivo con otro negativo cuyo resultado es
+		// negativo
 		assertEquals(new Integer(-2), Calculadora.suma(-5, 3));
-		
+
 		// Comprobamos la suma de dos números negativos
-		assertEquals(new Integer(-8), Calculadora.suma(-5, -3));			
+		assertEquals(new Integer(-8), Calculadora.suma(-5, -3));
 	}
-	
+
 	@Test
 	public void testResta() {
 		// Comprobamos la resta básica
 		assertEquals(new Integer(3), Calculadora.resta(5, 2));
-				
-		// Comprobamos la resta de un número positivo con otro negativo cuyo resultado es positivo
+
+		// Comprobamos la resta de un número positivo con otro negativo cuyo resultado
+		// es positivo
 		assertEquals(new Integer(8), Calculadora.resta(5, -3));
-				
+
 		// Comprobamos la reta de dos números negativos
-		assertEquals(new Integer(-2), Calculadora.resta(-5, -3));			
+		assertEquals(new Integer(-2), Calculadora.resta(-5, -3));
 	}
-	
+
 	@Test
 	public void testMultiplicacion() {
 		// Comprobamos la multiplicación básica
-		assertEquals(new Integer(10), Calculadora.multiplica(5, 2));		
-		
+		assertEquals(new Integer(10), Calculadora.multiplica(5, 2));
+
 		// Comprobamos que la multiplicación por 0 da 0
 		assertEquals(new Integer(0), Calculadora.multiplica(5, 0));
-		
+
 		// Comprobamos que la multiplicación por 1 da el mismo número
 		assertEquals(new Integer(5), Calculadora.multiplica(5, 1));
 
 		// Comprobamos la multiplicación de un número positivo por uno negativo
-		assertEquals(new Integer(-15), Calculadora.multiplica(-5, 3));		
+		assertEquals(new Integer(-15), Calculadora.multiplica(-5, 3));
 
 		// Comprobamos la multiplicación de dos números negativos
-		assertEquals(new Integer(15), Calculadora.multiplica(-5, -3));		
-		
+		assertEquals(new Integer(15), Calculadora.multiplica(-5, -3));
+
 		// Comprobamos la propiedad conmutativa
-		assertEquals(Calculadora.multiplica(3, 5), Calculadora.multiplica(5, 3));		
+		assertEquals(Calculadora.multiplica(3, 5), Calculadora.multiplica(5, 3));
 	}
-	
+
 	@Test
 	public void testDivision() {
 		// Comprobamos la división exacta
 		assertEquals(new Double(2), Calculadora.divide(4, 2));
-		
+
 		// Comprobamos la división con decimales
 		assertEquals(new Double(2.5), Calculadora.divide(5, 2));
-		
+
 		// Comprobamos la divisón por cero
 		assertEquals(Double.valueOf(Double.POSITIVE_INFINITY), Calculadora.divide(5, 0));
-		
-		// Comprobamos la división con decimales, de un número positivo con otro negativo
-		assertEquals(new Double(-2.5), Calculadora.divide(5, -2));		
+
+		// Comprobamos la división con decimales, de un número positivo con otro
+		// negativo
+		assertEquals(new Double(-2.5), Calculadora.divide(5, -2));
 
 		// Comprobamos la división con decimales, de dos números positivos
-		assertEquals(new Double(2.5), Calculadora.divide(-5, -2));				
-		
+		assertEquals(new Double(2.5), Calculadora.divide(-5, -2));
+
 	}
-	
+
 	@Test
 	public void testRaizCuadrada() {
 		// Comprobamos la raiz cuadrada básica
 		assertEquals(new Double(3), Calculadora.raizCuadrada(9));
-		
-		// Comprobamos la raiz cuadrada que da de resultado un decimal. Como la precisión debe ser mayor a 10-5 supongo 6 decimales.
+
+		// Comprobamos la raiz cuadrada que da de resultado un decimal. Como la
+		// precisión debe ser mayor a 10-5 supongo 6 decimales.
 		assertEquals(new Double(2.236068), Calculadora.raizCuadrada(5));
-		
-		// Comprobamos que la raiz cuadrada de un número negativo no se intenta calcular y devuelve null
+
+		// Comprobamos que la raiz cuadrada de un número negativo no se intenta calcular
+		// y devuelve null
 		assertNull(Calculadora.raizCuadrada(-5));
 	}
 }
