3b5b49f7cc670bf7eceb9c8423c0d18a2975ba92
Arturo Llavona Vallina <allavona@paradigmadigital.com>
2020-05-22 18:54:07 +0200

    Implementamos el helper de la calculadora, que calcula el resultado a partir de la entrada del usuario.

--- src/main/java/net/unir/allavona/isa/CalculadoraHelper.java
+++ src/main/java/net/unir/allavona/isa/CalculadoraHelper.java
@@ -1,7 +1,50 @@
 package net.unir.allavona.isa;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CalculadoraHelper {
+	private static String PATTERN_2OP = "^\\s*(\\-?\\d+)\\s*(\\+|-|:|/|\\*)\\s*(\\-?\\d+)\\s*";
+	private static String PATTERN_RAIZ = "^\\s*raiz\\s*(\\-?\\d+)\\s*";
+	
 	public static String calcular(String cadena) {
-		return null;
+		String result = "";
+		Pattern p = Pattern.compile(PATTERN_2OP);
+		Matcher m = p.matcher(cadena);
+		
+		if ( m.find() ) {
+			String n1 = m.group(1);
+			String op = m.group(2);
+			String n2 = m.group(3);
+			
+			switch ( op ) {
+				case "+" :
+					result = String.valueOf(Calculadora.suma(Integer.valueOf(n1), Integer.valueOf(n2)));
+					break;
+				case "-" :
+					result = String.valueOf(Calculadora.resta(Integer.valueOf(n1), Integer.valueOf(n2)));
+					break;
+				case "*" :
+					result = String.valueOf(Calculadora.multiplica(Integer.valueOf(n1), Integer.valueOf(n2)));
+					break;
+				case ":":
+				case "/":
+					result = String.valueOf(Calculadora.divide(Integer.valueOf(n1), Integer.valueOf(n2)));
+					break;
+				default :
+					result = Constantes.ERROR_ENTRADA_NO_RECONOCIDA;
+			}						
+		} else {
+			p = Pattern.compile(PATTERN_RAIZ);
+			m = p.matcher(cadena);
+			
+			if ( m.find() ) {
+				String n = m.group(1);
+				result = String.valueOf(Calculadora.raizCuadrada(Integer.valueOf(n)));
+			} else {
+				result = Constantes.ERROR_ENTRADA_NO_RECONOCIDA;
+			}
+		}			
+		return result;
 	}
 }
